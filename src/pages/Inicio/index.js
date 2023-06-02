76111469957c350e77db22846f1734c94f357993
renanlucena <renan.segura.de.lucena@gmail.com>
2023-06-02 10:41:20 -0300

    aplicando Cabecalho e Rodape para todas as rotas

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,7 +1,5 @@
 import Banner from "components/Banner";
-import Cabecalho from "components/Cabecalho";
 import Cards from "components/Cards";
-import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
 
 
@@ -9,14 +7,14 @@ export default function Inicio(){
     return(
         <>
         
-        <Cabecalho/>
+        
         <Banner imagem="home" />
         <Titulo>
             <h1>Um lugar para guardar seus vídeos e filmes!</h1>
         </Titulo>
         <Cards/>
         
-        <Rodape />
+   
         </>
         )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,5 @@
+import Cabecalho from "components/Cabecalho";
+import Rodape from "components/Rodape";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -5,10 +7,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 export default function AppRoutes(){
     return(
         <BrowserRouter>
+        <Cabecalho />
             <Routes>
                 <Route path="/" element={<Inicio/>}></Route>
                 <Route path="/favoritos" element={<Favoritos/>}></Route>
             </Routes>
+        <Rodape />
         </BrowserRouter>
     )
 }
