69ff42ce4de8fef56fd4ded285ceb9486e72fd74
renanlucena <renan.segura.de.lucena@gmail.com>
2023-06-02 13:24:39 -0300

    implementando a página de Favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import styles from "./Favoritos.module.css";
+import Banner from 'components/Banner';
+import Titulo from 'components/Titulo';
+import videos from "json/db.json";
+import Card from "components/Card";
 
 export default function Favoritos() {
+    const favoritos = videos[2];
   return (
     <>
-    
+        <Banner imagem="favoritos" />
+        <Titulo>
+            <h1>Meus Favoritos</h1>
+        </Titulo>
+        <section className={styles.container}>
+            <Card {...favoritos}/>
+        </section>
     </>
     
     )
